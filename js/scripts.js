fb805d79a24fbd7a7a760fff6e758ff5d273d2c9
Anurag Shamsher Rana <anurag@sherpa-tech.com>
2024-02-15 01:22:50 +0545

    initial

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-// side nav
-const sideNavButton = document.querySelector(".side-nav-button");
-const sideNav = document.querySelector(".side-nav");
-const sideNavBg = document.querySelector(".side-nav-bg");
-const sideNavClose = document.querySelector(".side-nav-close");
-const sideNavItems = document.querySelectorAll(".side-nav .nav-button");
-
-sideNavButton.addEventListener("click", () => {
-  sideNav.classList.add("active");
-});
-
-const closeSideNav = () => {
-  sideNav.classList.remove("active");
-};
-
-sideNavClose.addEventListener("click", closeSideNav);
-sideNavBg.addEventListener("click", closeSideNav);
-
-sideNavItems.forEach((sideNavItem) => {
-  sideNavItem.addEventListener("click", closeSideNav);
-});
-
 const header = document.querySelector("header");
 
 document.querySelector("main").addEventListener("scroll", (e) => {
